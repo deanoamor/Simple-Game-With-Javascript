18c9ee590807916de1a39ac24ba80f5997395e6c
deanoamor <toonoob4@gmail.com>
2022-05-25 17:26:21 +0700

    add heal feature

--- script.js
+++ script.js
@@ -8,23 +8,56 @@ let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 let dateTime = date+' '+time;
 
-function Character(nama,role,skill){
-    this.nama = nama;
-    this.role = role;
-    this.skill = skill;
-    this.energy = 100;
-    this.hp = 100;
-    this.inventory = [["Health potion" , 50 , 5] , ["Energy potion", 50, 5]];
-    this.status = "alive";
 
-    this.walking = function(){
+// function Character(nama,role,skill){
+//     this.nama = nama;
+//     this.role = role;
+//     this.skill = skill;
+//     this.energy = 100;
+//     this.hp = 100;
+//     this.inventory = [["Health potion" , 50 , 5] , ["Energy potion", 50, 5]];
+//     this.status = "alive";
+// }
+
+// Character.prototype.walking = function(){
+//     this.energy = this.energy - 5;
+// }
+
+// Character.prototype.attack = function(attackPower){
+//     this.energy = this.energy - attackPower;
+// }
+
+class Character{
+    constructor(nama,role,skill){
+        this.nama = nama;
+        this.role = role;
+        this.skill = skill;
+        this.energy = 100;
+        this.hp = 100;
+        this.inventory = [["Health potion" , 50 , 5] , ["Energy potion", 50, 5]];
+        this.status = "alive";
+    }
+
+    walking(){
         this.energy = this.energy - 5;
     }
-
-    this.attack = function(attackPower){
+    
+    attack(attackPower){
         this.energy = this.energy - attackPower;
     }
+
+    healhp(){
+        this.hp = this.hp + 50;
+        this.inventory[0][2] = this.inventory[0][2] - 1;
+    }
+
+    healenergy(){
+        this.energy = this.energy + 50;
+        this.inventory[1][2] = this.inventory[1][2] - 1;
+    }
+
 }
+
 
 function randomNumber (){
     return Math.floor(Math.random() * 10);
@@ -49,7 +82,7 @@ while(ulang){
     for(let i = 0; i < 1; i++){
         characterCreate[help] = new Character(nama,role,skill);
         alert("Character name " + characterCreate[help].nama + " with role " + characterCreate[help].role + " with skill " + characterCreate[help].skill);
-        console.log("[" + dateTime + "] " + (help + 1) + ".Character name " + characterCreate[help].nama + " with role " + characterCreate[help].role + " with skill " + characterCreate[help].skill);
+        console.log((help + 1) + ".Character name " + characterCreate[help].nama + " with role " + characterCreate[help].role + " with skill " + characterCreate[help].skill);
     }
     help++;
 
@@ -77,18 +110,23 @@ if(characterCreate.length < 2){
 
 alert("your character is " + characterCreate[select].nama + " with role " + characterCreate[select].role + " with skill " + characterCreate[select].skill);
 alert("your character energy is " + characterCreate[select].energy);
-console.log("[" + dateTime + "] " + "your character energy is " + characterCreate[select].energy);
+console.log("your character energy is " + characterCreate[select].energy);
+
 alert("your character hp is " + characterCreate[select].hp);
-console.log("[" + dateTime + "] " + "your character hp is " + characterCreate[select].hp);
+console.log("your character hp is " + characterCreate[select].hp);
+
 alert("your character inventory are " + characterCreate[select].inventory[0][0] + " will increase " + characterCreate[select].inventory[0][1] + " health (" + characterCreate[select].inventory[0][2] + " item and " 
 + characterCreate[select].inventory[1][0] + " will increase " + characterCreate[select].inventory[1][1] + " energy (" + characterCreate[select].inventory[1][2] + " item)");
+console.log("your character inventory are " + characterCreate[select].inventory[0][0] + " will increase " + characterCreate[select].inventory[0][1] + " health (" + characterCreate[select].inventory[0][2] + " item and "
++ characterCreate[select].inventory[1][0] + " will increase " + characterCreate[select].inventory[1][1] + " energy (" + characterCreate[select].inventory[1][2] + " item)");
+
 alert("your character status is " + characterCreate[select].status);
-console.log("[" + dateTime + "] " + "your character status is " + characterCreate[select].status);
+console.log("your character status is " + characterCreate[select].status);
 
 
-alert("There are 3 branches of the road, where are you going?");
-alert("(1.Forest) - (2.Castle) - (3.market)");
-console.log("[" + dateTime + "] " + "(1.Forest) - (2.Castle) - (3.market)");
+alert("There are 2 branches of the road, where are you going?");
+alert("(1.Forest) - (2.Castle)");
+console.log("(1.Forest) - (2.Castle)");
 
 let choosePlace = parseInt(prompt("Choose place (according to the number)"));
 switch(choosePlace){
@@ -97,7 +135,7 @@ switch(choosePlace){
         characterCreate[select].walking();
         alert("you are walking and it drains your energy");
         alert("your energy now " + characterCreate[select].energy);
-        console.log("[" + dateTime + "] " + "your energy now " + characterCreate[select].energy);
+        console.log("your energy now " + characterCreate[select].energy);
         let chooseEnemyForest = confirm("there is a enemy, want to fight?");
         if(chooseEnemyForest == true){
         
@@ -105,32 +143,49 @@ switch(choosePlace){
 
         alert("you are fighting with enemy");
         alert("enemy hp " + enemyForestHp);
-        console.log("[" + dateTime + "] " + "enemy hp " + enemyForestHp);
+        console.log("enemy hp " + enemyForestHp);
             while(enemyForestHp > 1){
                 
                 let attackEnemyForest = parseInt(prompt("How much power do you want to use?"));
+
                 alert("you attack with " + attackEnemyForest + " power");
                 characterCreate[select].attack(attackEnemyForest);
                 alert("your energy now " + characterCreate[select].energy);
-                console.log("[" + dateTime + "] " + "your energy now " + characterCreate[select].energy);
+                console.log("your energy now " + characterCreate[select].energy);
                 enemyForestHp = enemyForestHp - attackEnemyForest;
                 alert("enemy hp now " + enemyForestHp);
-                console.log("[" + dateTime + "] " + "enemy hp now " + enemyForestHp);
+                console.log("enemy hp now " + enemyForestHp);
 
                 let enemyPowerForest = enemyPowerRandom();
                 let randomNumberForest = randomNumber();
                 console.log(randomNumberForest);
 
-                if(randomNumberForest > 5){
-                    alert("oh no, the enemy attacks you with " + enemyPowerForest);
-                    console.log("[" + dateTime + "] " + "oh no, the enemy attacks you with " + enemyPowerForest);
-                    characterCreate[select].hp = characterCreate[select].hp - enemyPowerForest;
-                    alert("your hp now " + characterCreate[select].hp);
-                    console.log("[" + dateTime + "] " + "your hp now " + characterCreate[select].hp);
+                if(enemyForestHp > 0){
+                    if(randomNumberForest > 5){
+                        alert("oh no, the enemy attacks you with " + enemyPowerForest);
+                        console.log("oh no, the enemy attacks you with " + enemyPowerForest);
+                        characterCreate[select].hp = characterCreate[select].hp - enemyPowerForest;
+                        alert("your hp now " + characterCreate[select].hp);
+                        console.log("your hp now " + characterCreate[select].hp);
+                        if(characterCreate[select].hp <=50){
+                            alert("you are too weak");
+                            let usePotion = parseInt(prompt("want to use poison? (1.yes, 2.no)"));
+                            if(usePotion === 1){  
+                                characterCreate[select].healhp();
+                                alert("you are healed and your hp is " + characterCreate[select].hp);
+                                console.log("you are healed and your hp is " + characterCreate[select].hp);
+                                alert("there are " + characterCreate[select].inventory[0][2] + " HP potion left in your inventory");
+                                console.log("there are " + characterCreate[select].inventory[0][2] + " HP potion left in your inventory");
+                            }else{
+                                alert("you continue to fight");
+                            }
+                        }
+                    }
                 }
             }
         alert("congratulations!! you killed the enemy");
         }
+        alert("you continue walking");
     
         break;
 }
